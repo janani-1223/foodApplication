5ba221494f653196efcbe5a30062c98881f2fc97
janani-1223 <jananilavanya01@gmail.com>
2021-04-26 12:15:20 +0530

    first commit1

--- raj_app/src/components/Home.js
+++ raj_app/src/components/Home.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react';
-
+import './Home.css';
 class Home extends Component {
     render() {
         return (
-            <div>
-                <h1>Hello React</h1>
-            </div>
+            
+        <nav class="navbar navbar-expand-sm bg-light navbar-light fixed-top sticky-top">
+            <a class="navbar-brand" href="food_logo.png"></a>
+                <img src="food_logo.png" alt="my_logo" width="100px" /> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                
+
+                <form class="form-inline justify-content-center" text-align= "center">
+      <input class="form-control mr-sm-2"type="text" placeholder="Search" aria-label="Search"></input>
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+                <ul class="navbar-nav ml-auto">
+    <li class="nav-item">
+      <a class="nav-link" href="/login" >Login</a>
+    </li>&nbsp;&nbsp;
+    <li class="nav-item">
+      <a class="nav-link" href="/signup">Sign Up</a>
+    </li>
+  </ul>
+  </nav>
+           
         );
     }
 }
